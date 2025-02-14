fd144e6826db9f84c3ebac220ccce03dc55210b2
SanqDuonq <namsang0902s@gmail.com>
2025-02-14 21:44:13 +0700

    set up init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+    return (
+        <>
+            <div className=" text-red-500 text-3xl">
+                Welcome admin!
+            </div>
+        </>
+    )
+}
+
+export default App
