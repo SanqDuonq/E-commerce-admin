ac3cbdbf9c356332121b414333b2c1f59f73bfe1
baongoc2211 <hoavienchon@gmai.com>
2025-02-17 23:05:02 +0700

    feat:side UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
+import Navbar from "./components/navbar";
+import Sidebar from "./components/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import Add from "./pages/add";
+import List from "./pages/list";
+import Orders from "./pages/orders";
+import { useState } from "react";
+import Login from "./components/login";
 function App() {
-
-    return (
+  const [token, setToken] = useState(``);
+  return (
+    <div className="bg-gray-50 min-h-screen">
+      {token === "" ? (
+        <Login />
+      ) : (
         <>
-            <div className=" text-red-500 text-3xl">
-                Welcome admin!
-            </div>
+          <Navbar />
+          <hr />
+          <div className="flex w-full">
+            <Sidebar />
+            <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base"></div>
+            <Routes>
+              <Route path="/add" element={<Add />} />
+              <Route path="/list" element={<List />} />
+              <Route path="/orders" element={<Orders />} />
+            </Routes>
+          </div>
         </>
-    )
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
