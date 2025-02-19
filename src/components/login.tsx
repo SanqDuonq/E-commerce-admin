b367a627b272e0c25421ebfa0342d5e716a617f3
baongoc2211 <hoavienchon@gmai.com>
2025-02-19 13:42:08 +0700

    feat:Sign In UI

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -6,6 +6,7 @@ const login = () => {
   const onsubmitHandler = async (e) => {
     try {
       e.preventDefaults();
+      console.log(email, password);
     } catch (error) {}
   };
   return (
@@ -18,6 +19,8 @@ const login = () => {
               Email Address
             </p>
             <input
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
               className="rounded-md w-full px-3 py-2 border border-gray-300 outline-none"
               type="email"
               placeholder="your@gmail.com"
@@ -27,6 +30,8 @@ const login = () => {
           <div>
             <p className="text-sm font-medium text-gray-700 mb-2">Password</p>
             <input
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
               className="rounded-md w-full px-3 py-2 border border-gray-300 outline-none"
               type="password"
               placeholder="Enter your password"
